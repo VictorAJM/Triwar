13a3b693bb974a989835aafba2dbd484eccffd02
VictorAJM <victorarmandojaramillo@gmail.com>
2022-10-15 14:20:09 -0500

    add worker-base movement

--- Movements.h
+++ Movements.h
@@ -62,13 +62,21 @@ void moveWorker(Worker* worker, vector<Worker*> workers, vector<Mineral*> minera
         }
         
         int myDistance = distance[worker->X()][worker->Y()];
+        if (myDistance <= 1) {
+            worker->paint();
+            worker->setGold(50);
+            return;
+        } 
         if (worker->X()>LEFT_MAP && distance[worker->X()-1][worker->Y()] < myDistance) {
             myDistance = distance[worker->X()-1][worker->Y()];
-        } else if (worker->X()<RIGHT_MAP && distance[worker->X()+1][worker->Y()] < myDistance) {
+        } 
+        if (worker->X()<RIGHT_MAP && distance[worker->X()+1][worker->Y()] < myDistance) {
             myDistance = distance[worker->X()+1][worker->Y()];
-        } if (worker->Y()>TOP_MAP && distance[worker->X()][worker->Y()-1] < myDistance) {
+        } 
+        if (worker->Y()>TOP_MAP && distance[worker->X()][worker->Y()-1] < myDistance) {
             myDistance = distance[worker->X()][worker->Y()-1];
-        } if (worker->Y()<BOT_MAP && distance[worker->X()][worker->Y()+1] < myDistance) {
+        } 
+        if (worker->Y()<BOT_MAP && distance[worker->X()][worker->Y()+1] < myDistance) {
             myDistance = distance[worker->X()][worker->Y()+1];
         } 
 
@@ -76,15 +84,97 @@ void moveWorker(Worker* worker, vector<Worker*> workers, vector<Mineral*> minera
             worker->setX(worker->X()-1);
         } else if (worker->X()<RIGHT_MAP && distance[worker->X()+1][worker->Y()] == myDistance) {
             worker->setX(worker->X()+1);
-        } if (worker->Y()>TOP_MAP && distance[worker->X()][worker->Y()-1] == myDistance) {
+        } else if (worker->Y()>TOP_MAP && distance[worker->X()][worker->Y()-1] == myDistance) {
             worker->setY(worker->Y()-1);
-        } if (worker->Y()<BOT_MAP && distance[worker->X()][worker->Y()+1] == myDistance) {
+        } else if (worker->Y()<BOT_MAP && distance[worker->X()][worker->Y()+1] == myDistance) {
             worker->setY(worker->Y()+1);
         } else {
 
         }
         worker->paint();
     } else {
+bool visited[150][40];
+        int distance[150][40];
+        const int inf = 1e9+7;
+        for (int i=0;i<150;i++) {
+            for (int j=0;j<40;j++) distance[i][j] = inf;
+        }
+        for (int i=LEFT_MAP;i<=RIGHT_MAP;i++) {
+            for (int j=TOP_MAP;j<=BOT_MAP;j++) visited[i][j] = false;
+        }
+        queue<pair<pair<int,int> ,int> > dist_to_base;
+        for (auto _mineral : minerals) {
+            int w = _mineral->W();
+            int h = _mineral->H();
+            for (int i=_mineral->X();i<_mineral->X()+w;i++) {
+                for (int j=_mineral->Y();j<_mineral->Y()+h;j++) {
+                    visited[i][j]=true;
+                }
+            }
+        }
+        for (auto _base : bases) {
+            int w = _base->W();
+            int h = _base->H();
+            for (int i=_base->X();i<_base->X()+w;i++) {
+                for (int j=_base->Y();j<_base->Y()+h;j++) {
+                    if (_base->RACE() == worker->RACE()) dist_to_base.push({{i,j},0});
+                    visited[i][j]=true;
+                }
+            }
+        }
+        for (auto _worker : workers) {
+            visited[_worker->X()][_worker->Y()] = true;
+        }
+        
+        while (!dist_to_base.empty()) {
+            auto dist = dist_to_base.front();
+            distance[dist.first.first][dist.first.second] = dist.second;
+            dist_to_base.pop();
+            if (dist.first.first>LEFT_MAP && !visited[dist.first.first-1][dist.first.second]) {
+                visited[dist.first.first-1][dist.first.second] = true;
+                dist_to_base.push({{dist.first.first-1,dist.first.second},dist.second+1});
+            } 
+            if (dist.first.first<RIGHT_MAP && !visited[dist.first.first+1][dist.first.second]) {
+                visited[dist.first.first+1][dist.first.second] = true;
+                dist_to_base.push({{dist.first.first+1,dist.first.second},dist.second+1});
+            } 
+            if (dist.first.second>TOP_MAP && !visited[dist.first.first][dist.first.second-1]) {
+                visited[dist.first.first][dist.first.second-1] = true;
+                dist_to_base.push({{dist.first.first,dist.first.second-1},dist.second+1});
+            } 
+            if (dist.first.second<BOT_MAP && !visited[dist.first.first][dist.first.second+1]) {
+                visited[dist.first.first][dist.first.second+1] = true;
+                dist_to_base.push({{dist.first.first,dist.first.second+1},dist.second+1});
+            } 
+        }
+        int myDistance = distance[worker->X()][worker->Y()];
+        if (myDistance <= 1) {
+            worker->paint();
+            worker->setGold(0);
+            return;
+        } 
+        if (worker->X()>LEFT_MAP && distance[worker->X()-1][worker->Y()] < myDistance) {
+            myDistance = distance[worker->X()-1][worker->Y()];
+        } 
+        if (worker->X()<RIGHT_MAP && distance[worker->X()+1][worker->Y()] < myDistance) {
+            myDistance = distance[worker->X()+1][worker->Y()];
+        } 
+        if (worker->Y()>TOP_MAP && distance[worker->X()][worker->Y()-1] < myDistance) {
+            myDistance = distance[worker->X()][worker->Y()-1];
+        }
+        if (worker->Y()<BOT_MAP && distance[worker->X()][worker->Y()+1] < myDistance) {
+            myDistance = distance[worker->X()][worker->Y()+1];
+        } 
 
+        if (worker->X()>LEFT_MAP && distance[worker->X()-1][worker->Y()] == myDistance) {
+            worker->setX(worker->X()-1);
+        } else if (worker->X()<RIGHT_MAP && distance[worker->X()+1][worker->Y()] == myDistance) {
+            worker->setX(worker->X()+1);
+        } else if (worker->Y()>TOP_MAP && distance[worker->X()][worker->Y()-1] == myDistance) {
+            worker->setY(worker->Y()-1);
+        } else if (worker->Y()<BOT_MAP && distance[worker->X()][worker->Y()+1] == myDistance) {
+            worker->setY(worker->Y()+1);   
+        }
+        worker->paint(); 
     }
 }
